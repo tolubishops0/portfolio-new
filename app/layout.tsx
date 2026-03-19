747cb7615ed0c879ca092c47a719f161b1249711
Tolu okunjoyo <toluokunjoyo@gmail.com>
2026-03-19 13:44:25 +0100

    chore:update procject stack

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { ParticleBackground } from "@/components/particle-component";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const fontsMain = Roboto({ subsets: ["latin"] });
+const fontSub = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tolu Bishop | Full-Stack Developer",
+  title: "ToluBishops",
   description:
-    "Full-stack developer specialising in React, Next.js, and modern web architecture. Building things for the web.",
+    "Software developer specialising in React, Next.js, and modern web architecture. Building things for the web.",
 
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
+        url: "/icon-darkk.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/icon-darkk.png",
   },
 };
 
@@ -39,7 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="font-sans antialiased bg-background text-foreground">
+      <body
+        className={`${fontsMain.className} ${fontSub.className} antialiased bg-background text-foreground`}
+      >
         <ParticleBackground />
         <ThemeProvider
           attribute="class"
